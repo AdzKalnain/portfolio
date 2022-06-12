2c145fb22767c51b3772d0a07054302cd6fc0c0a
Adzmi Kalnain <adzkalnain@gmail.com>
2022-06-12 11:46:30 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
