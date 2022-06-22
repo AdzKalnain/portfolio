5655b3471d0170b0456548e0ef19bc90639209fd
Adzmi Kalnain <adzkalnain@gmail.com>
2022-06-22 12:43:08 +0800

    Add files via upload

---
+++ js/typing.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    var typed = new Typed(".typing", {
+        strings: ["Adzmi Kalnain", "Web Developer", "Designer", "Freelancer"],
+        typespeed: 100,
+        backspeed: 60,
+        loop: true
+    });
+
+});
